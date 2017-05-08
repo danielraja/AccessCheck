bf3f3d744e5c7edcc6cee2eb687a7268e8a9bbea
dvairach <dvairach@DVAIRACH-BYQFC.partnet.cisco.com>
2017-05-08 16:22:09 +0530

    added files

---
+++ TestProject/src/main/java/com/ver/check/TestProject/App.java
@@ -0,0 +1,13 @@
+package com.ver.check.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
